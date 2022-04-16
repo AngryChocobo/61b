ee2ecbee21cfdfdffa327e995d5ef45033fc1364
AngryChocobo <1347237169@qq.com>
2022-04-16 12:18:03 +0800

    IntList get index value

--- hw0/IntListRun.java
+++ hw0/IntListRun.java
@@ -24,6 +24,19 @@ public class IntListRun {
             }
             return count;
         }
+
+        public int get(int i) {
+            if (i > size()) {
+                throw new Error("越界");
+            }
+            int index = 1;
+            IntList p = this;
+            while (index < i) {
+                p = p.rest;
+                index++;
+            }
+            return p.first;
+        }
     }
 
     public static void main(String[] args) {
@@ -33,5 +46,6 @@ public class IntListRun {
         L.rest.rest.rest = new IntList(44, null);
         System.out.println(L.size());
         System.out.println(L.iterativeSize());
+        System.out.println(L.get(2));
     }
 }
