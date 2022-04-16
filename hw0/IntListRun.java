4ed06a603226cd37295178d04edf783369a989fc
AngryChocobo <1347237169@qq.com>
2022-04-16 11:54:44 +0800

    iterativeSize

--- hw0/IntListRun.java
+++ hw0/IntListRun.java
@@ -14,22 +14,24 @@ public class IntListRun {
             }
             return 1 + rest.size();
         }
-    }
 
-    // public static int size(IntList L) {
-    // int count = 0;
-    // IntList current = L;
-    // while (current != null) {
-    // count++;
-    // current = current.rest;
-    // }
-    // return count;
-    // }
+        public int iterativeSize() {
+            int count = 0;
+            IntList current = this;
+            while (current != null) {
+                count++;
+                current = current.rest;
+            }
+            return count;
+        }
+    }
 
     public static void main(String[] args) {
         IntList L = new IntList(5, null);
         L.rest = new IntList(10, null);
         L.rest.rest = new IntList(15, null);
+        L.rest.rest.rest = new IntList(44, null);
         System.out.println(L.size());
+        System.out.println(L.iterativeSize());
     }
 }
